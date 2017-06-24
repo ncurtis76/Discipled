34da452fd2a932e08e3e45233af62623fd1e615b
Nathan Curtis <natecurtis@bellsouth.net>
2017-06-24 10:00:52 -0400

    added lights scaffold, updated navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  resources :lights
+  devise_for :users
+  root 'home#index'
   get 'home/index'
 
-  root 'home#index'
+
+  get 'home/about'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
